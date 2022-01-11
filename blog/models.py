7868f1a00997d60894e88699bce88aeca1689264
dura-amar <dura.email@gmail.com>
2022-01-11 22:10:08 +0545

    models created

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.template.defaultfilters import slugify
 
 # Create your models here.
+
+class Category(models.Model):
+    name=models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+class BlogPost(models.Model):
+    title=models.CharField(max_length=50)
+    date_posted=models.DateTimeField(auto_now_add=True)
+    author=models.ForeignKey(User)
+    category=models.ForeignKey(Category)
+    thumbnail=models.ImageField()
+    slug_title=models.SlugField(null=False,blank=False,editable=False)
+
+    def __str__(self):
+        return self.title
+    
+    def save(self, *args, **kwargs):
+        self.slug_title = slugify(self.title)
+        super(BlogPost, self).save(*args, **kwargs)
